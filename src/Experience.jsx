1e7702cfa0b4556b63f6a92e55da355dd7c3c630
Houmahani Kane <houmahanikane@gmail.com>
2023-04-11 16:24:09 +0200

    Last touches

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,16 +1,23 @@
-import { Environment, Html, OrbitControls, SpotLight } from '@react-three/drei'
+import { Environment, Html, Lightformer, OrbitControls, SpotLight } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import Room from './Room.jsx'
 import { Suspense } from 'react'
+import { useControls } from 'leva'
 
 export default function Experience() {
+  const { color } = useControls({
+    color: '#674e41'
+  })
+
   return (
     <>
       <Perf position='top-left' />
 
-      <OrbitControls makeDefault />
-      <directionalLight color={'red'} intensity={10} />
-      <color args={['#674e41']} attach={'background'} />
+      <OrbitControls enableZoom enablePan={false} enableRotate={false} />
+
+      <color args={[color]} attach={'background'} />
+
+      <ambientLight intensity={1.5} />
 
       <Suspense>
         <Room />
--- src/Room.jsx
+++ src/Room.jsx
@@ -1,34 +1,33 @@
-import { Center, useGLTF, useTexture } from '@react-three/drei'
+import { Center, PresentationControls, useGLTF, useTexture } from '@react-three/drei'
 
 export default function Room() {
   const { nodes } = useGLTF('./modern-living-room.glb')
   const bakedTexture = useTexture('./baked.jpg')
   bakedTexture.flipY = false
 
-  console.log(nodes)
-
   return (
-    <Center>
-      <mesh geometry={nodes.baked.geometry} scale={0.3}>
-        <meshBasicMaterial map={bakedTexture} />
-      </mesh>
-      <mesh
-        geometry={nodes.window.geometry}
-        scale={0.3}
-        position={[-0.05, -0.05, nodes.window.position.z]}
-      >
-        <meshBasicMaterial color={'#FFA868'} />
-      </mesh>
+    <PresentationControls zoom={0.8} polar={[-0.4, 0.4]} azimuth={[-0.5, 0.5]}>
+      <Center>
+        <mesh geometry={nodes.baked.geometry} scale={0.3}>
+          <meshBasicMaterial map={bakedTexture} />
+        </mesh>
+        <mesh
+          geometry={nodes.window.geometry}
+          scale={0.3}
+          position={[-0.05, -0.05, nodes.window.position.z]}
+        >
+          <meshBasicMaterial color={'#FFA868'} />
+        </mesh>
 
-      <mesh
-        geometry={nodes.window.geometry}
-        scale={0.3}
-        position={[-0.05, nodes.window.position.z, nodes.window.position.z]}
-      >
-        <meshBasicMaterial color={'#FFA868'} />
-      </mesh>
+        <mesh
+          geometry={nodes.window.geometry}
+          scale={0.3}
+          position={[-0.05, nodes.window.position.z, nodes.window.position.z]}
+        >
+          <meshBasicMaterial color={'#FFA868'} />
+        </mesh>
 
-      {/* <mesh
+        {/* <mesh
         geometry={nodes.Glass.geometry}
         position={nodes.Glass.position}
         scale={0.3}
@@ -37,7 +36,8 @@ export default function Room() {
       >
         <meshBasicMaterial color={'#FFA868'} />
       </mesh> */}
-    </Center>
+      </Center>
+    </PresentationControls>
   )
 }
 
