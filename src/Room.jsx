f29e9e2c49a9084b1beae98121fa9334a1ddab2a
Houmahani Kane <houmahanikane@gmail.com>
2023-05-01 19:21:45 +0200

    Reduces file size

--- src/Room.jsx
+++ src/Room.jsx
@@ -6,14 +6,7 @@ export default function Room() {
   const { nodes } = useGLTF('./modern-living-room.glb')
   const bakedTexture = useTexture('./baked.jpg')
   bakedTexture.flipY = false
-  const { width } = useThree((state) => state.viewport)
-  useFrame((state, delta) => {
-    if (state.viewport.width <= 3.8) {
-      state.camera.fov = 75
-    } else {
-      state.camera.fov = 45
-    }
-  })
+
   return (
     <>
       <group position-y={-0.5} dispose={null}>
